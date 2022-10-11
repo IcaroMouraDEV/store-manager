e726238bcbdf85e79d5a02dfb438470b546b7b0a
Icaro Joel <icaromoura.programador@gmail.com>
2022-10-11 17:47:12 -0300

    model 100%

--- src/models/connection.js
+++ src/models/connection.js
@@ -1,13 +1,12 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-// connection with mysqlDataBase
 const connection = mysql.createPool({
   host: process.env.MYSQL_HOST,
   port: 3306,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE || 'StoreManager',
+  database: 'StoreManager',
 });
 
 module.exports = connection;
---
+++ tests/unit/models/product.test.js
@@ -0,0 +1,21 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+const { productModel } = require('../../../src/models');
+const connection = require('../../../src/models/connection');
+const { products } = require('./mocks/productMock');
+
+describe('Testes de unidade do model de pessoas passageiras', function () {
+  afterEach(sinon.restore);
+
+  it('Recuperando a lista de pessoas passageiras', async function () {
+    sinon.stub(connection, 'execute').resolves([products]);
+    const result = await productModel.findAll();
+    expect(result).to.be.deep.equal(products);
+  });
+  it('Recuperando a lista de pessoas passageiras', async function () {
+    sinon.stub(connection, 'execute').resolves([[products[0]]]);
+    const result = await productModel.findById(1);
+    expect(result).to.be.deep.equal(products[0]);
+  });
+});
