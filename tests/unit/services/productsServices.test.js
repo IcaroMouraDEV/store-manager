c9724a568a50e6b2a1aa2938400473866f30da21
Icaro Joel <icaromoura.programador@gmail.com>
2022-10-13 15:38:09 -0300

    test fail fuck

--- tests/unit/services/productsServices.test.js
+++ tests/unit/services/productsServices.test.js
@@ -7,30 +7,28 @@ const { products, productUpdated, updatedProduct } = require('./mocks/productMoc
 describe('Testes de unidade do Service de produtos', function () {
   afterEach(sinon.restore);
 
-  describe('Testando o endpoint get de products', function () {
-    it('Recuperando a lista de produtos', async function () {
-      sinon.stub(productModel, 'findAll').resolves(products);
-      const result = await productService.findAllProduct();
-        
-      expect(result.type).to.equal(null);
-      expect(result.message).to.be.deep.equal(products);
-    });
-    
-    it('Recuperando a lista de produtos', async function () {
-      sinon.stub(productModel, 'findById').resolves(products[0]);
-      const result = await productService.findProductById(1);
-    
-      expect(result.type).to.equal(null);
-      expect(result.message).to.be.deep.equal(products[0]);
-    });
-    
-    it('Recuperando a lista de produtos com id inexistente', async function () {
-      sinon.stub(productModel, 'findById').resolves(products[4]);
-      const result = await productService.findProductById(4);
-    
-      expect(result.type).to.equal('error');
-      expect(result.message).to.be.deep.equal('Product not found');
-    });
+  it('Recuperando a lista de produtos', async function () {
+    sinon.stub(productModel, 'findAll').resolves(products);
+    const result = await productService.findAllProduct();
+      
+    expect(result.type).to.equal(null);
+    expect(result.message).to.be.deep.equal(products);
+  });
+  
+  it('Recuperando a lista de produtos', async function () {
+    sinon.stub(productModel, 'findById').resolves(products[0]);
+    const result = await productService.findProductById(1);
+  
+    expect(result.type).to.equal(null);
+    expect(result.message).to.be.deep.equal(products[0]);
+  });
+  
+  it('Recuperando a lista de produtos com id inexistente', async function () {
+    sinon.stub(productModel, 'findById').resolves(products[4]);
+    const result = await productService.findProductById(4);
+  
+    expect(result.type).to.equal('error');
+    expect(result.message).to.be.deep.equal('Product not found');
   });
 
   it('Cadastrando com inputs validos', async function () {
