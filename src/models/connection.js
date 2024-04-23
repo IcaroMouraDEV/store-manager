c1aa6e4860e9a015b9148be162e132b6aae217fc
Icaro Joel <icaromoura.programador@gmail.com>
2024-04-23 15:16:03 -0300

    add

--- src/models/connection.js
+++ src/models/connection.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const connection = mysql.createPool({
   host: process.env.MYSQL_HOST,
-  port: 3306,
+  port: 3300,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: 'StoreManager',
